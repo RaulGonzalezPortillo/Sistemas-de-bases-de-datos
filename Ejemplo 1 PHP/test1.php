d459f067001fa8127c23782beaa037707717fb9c
Raúl González Portillo <raulgonzalezportillo@gmail.com>
2020-03-31 02:34:45 -0600

    Ejemplo 1 PHP añadido

---
+++ Ejemplo 1 PHP/test1.php
@@ -0,0 +1,9 @@
+
+<?php
+	$userName = $_GET["userName"];
+	$pass = $_GET["formPassword1"];
+	$pass2 = $_GET["formPassword2"];
+	print "El userName es: $userName<br>";
+	print "El password es: $pass<br>";
+	print "El password 2 es: $pass2<br>";
+?>
